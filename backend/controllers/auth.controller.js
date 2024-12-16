9a793d85839cd201b9bdb88c323d2c57f666ca00
Shuva Kharel <shuvakharel007@gmail.com>
2024-12-16 17:09:11 +0545

    Auth Full + Sharehood Backend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,56 +1,60 @@
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
 import crypto from "crypto";
-import { generateTokenAndSetCookie } from '../utils/generateTokenAndSetCookie.js';
+
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
 import {
-    sendPasswordResetEmail,
+	sendPasswordResetEmail,
 	sendResetSuccessEmail,
 	sendVerificationEmail,
 	sendWelcomeEmail,
 } from "../mailtrap/emails.js";
-import { User } from '../models/user.model.js';
+import { User } from "../models/user.model.js";
 
 export const signup = async (req, res) => {
-    const {email, password, name} = req.body;
-    
-    try {
-        if(!email || !password || !name){
-            throw new Error("All fields are required");
-        }    
+	const { email, password, name } = req.body;
 
-        const userAlreadyExist = await User.findOne({email});
-        if(userAlreadyExist){
-            return res.status(400).json({success:false, message: "User already exists"});
-        }
+	try {
+		if (!email || !password || !name) {
+			throw new Error("All fields are required");
+		}
 
-        const hashedPassword = await bcryptjs.hash(password, 10);
-        const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+		const userAlreadyExists = await User.findOne({ email });
+		console.log("userAlreadyExists", userAlreadyExists);
 
-        const user = new User({
-            email,
-            password: hashedPassword,
-            name,
-            verificationToken,
-            verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000
-        })
+		if (userAlreadyExists) {
+			return res.status(400).json({ success: false, message: "User already exists" });
+		}
 
-        await user.save();
-        
-        generateTokenAndSetCookie(res, user._id);
+		const hashedPassword = await bcryptjs.hash(password, 10);
+		const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
 
-        await sendVerificationEmail(user.email, verificationToken);
-        
-        res.status(201).json({
-            success: true,
-            message: "User created successfully",
-            user: {
-                ...user._doc,
-                password: undefined
-            },
-        });
-    } catch (error) {
-        res.status(400).json({success: false, message: error.message});
-    }
-}
+		const user = new User({
+			email,
+			password: hashedPassword,
+			name,
+			verificationToken,
+			verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, // 24 hours
+		});
+
+		await user.save();
+
+		// jwt
+		generateTokenAndSetCookie(res, user._id);
+
+		await sendVerificationEmail(user.email, verificationToken);
+
+		res.status(201).json({
+			success: true,
+			message: "User created successfully",
+			user: {
+				...user._doc,
+				password: undefined,
+			},
+		});
+	} catch (error) {
+		res.status(400).json({ success: false, message: error.message });
+	}
+};
 
 export const verifyEmail = async (req, res) => {
 	const { code } = req.body;
@@ -86,7 +90,7 @@ export const verifyEmail = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-    const { email, password } = req.body;
+	const { email, password } = req.body;
 	try {
 		const user = await User.findOne({ email });
 		if (!user) {
@@ -114,12 +118,12 @@ export const login = async (req, res) => {
 		console.log("Error in login ", error);
 		res.status(400).json({ success: false, message: error.message });
 	}
-}
+};
 
 export const logout = async (req, res) => {
-    res.clearCookie("token");
-    res.status(200).json({ success: true, message: "Logged out successfully" });
-}
+	res.clearCookie("token");
+	res.status(200).json({ success: true, message: "Logged out successfully" });
+};
 
 export const forgotPassword = async (req, res) => {
 	const { email } = req.body;
